22333862c04811ce9d2b74cce1675bf74ca31e55
kuzmabr <kuzmabr@gmail.com>
2023-02-12 20:41:34 -0800

    loader

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { sleep, check } from 'k6';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
-  vus: 500,
+  vus: 1,
   duration: '20s',
 };
 export default function () {
@@ -12,26 +12,30 @@ export default function () {
   };
 
   const rndInt = randomIntFromInterval(900000, 1000000)
+
+  let res = http.get(`http://34.210.236.200/reviews?product_id=${rndInt}`);
+
+
     // http.get(`http://localhost:3000/reviews`);
     // let res = http.get(`http://localhost:3000/reviews?product_id=${rndInt}`);
     // let res = http.put(`http://localhost:3000/reviews/helpful?review_id=${rndInt}`)
 
-      let reviewObj = {
-      product_id: 1,
-      rating: 4,
-      summary: "Great product!",
-      recommend: true,
-      body: "I LOVE THIS.",
-      name: "Chonky",
-      photos: [],
-      characteristics: []
-      };
+      // let reviewObj = {
+      // product_id: 1,
+      // rating: 4,
+      // summary: "Great product!",
+      // recommend: true,
+      // body: "I LOVE THIS.",
+      // name: "Chonky",
+      // photos: [],
+      // characteristics: []
+      // };
 
-      let res = http.post("http://localhost:3000/reviews", JSON.stringify(reviewObj), {
-      headers: {
-      "Content-Type": "application/json"
-      }
-      });
+      // let res = http.post("http://localhost:3000/reviews", JSON.stringify(reviewObj), {
+      // headers: {
+      // "Content-Type": "application/json"
+      // }
+      // });
 
       // var res = http.get(`http://localhost:3000/reviews/metadata?product_id=${rndInt}`)
 
